6e63ad6db1ba971bf6eb72ec4be398e527f8fdde
ArthurJCQ <arthurjcq@hotmail.fr>
2021-12-08 21:08:04 +0100

    :rocket: Update knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,18 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
-const { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD, DB_PORT } = process.env;
-
 module.exports = {
   client: 'pg',
   version: '13',
-  connection: {
-    database: DB_NAME,
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: parseInt(DB_PORT, 10),
-  },
+  connection: process.env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     tableName: 'knex_migrations',
