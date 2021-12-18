1e3ecbbe9c8589ba3423667fa3e76d18d57df510
ArthurJCQ <arthurjcq@hotmail.fr>
2021-12-18 12:34:46 +0100

    :label: Update Player decorator types

--- src/modules/player/decorators/player.decorator.ts
+++ src/modules/player/decorators/player.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const Player = createParamDecorator(
-  (data: unknown, context: ExecutionContext) => {
+  (data: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
 
     return request.currentPlayer;
