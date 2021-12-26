c4638d16558a15d117a9bbf9d5fb436bd9314a83
ArthurJCQ <arthurjcq@hotmail.fr>
2021-12-26 23:37:55 +0100

    :sparkles: Ajout de fichiers de seeds

---
+++ knex/seeds/2_player_seed.ts
@@ -0,0 +1,36 @@
+import { Knex } from 'knex';
+
+import {
+  PLAYER,
+  PlayerRole,
+  PlayerStatus,
+} from '../../src/modules/player/constants';
+
+export async function seed(knex: Knex): Promise<void> {
+  // Deletes ALL existing entries
+  await knex(PLAYER).del();
+
+  // Inserts seed entries
+  await knex(PLAYER).insert([
+    {
+      id: 1,
+      name: 'Roco',
+      status: PlayerStatus.ALIVE,
+      role: PlayerRole.ADMIN,
+      roomId: 1,
+      passcode: 1234,
+      targetId: null,
+      missionId: null,
+    },
+    {
+      id: 2,
+      name: 'Sifredi',
+      status: PlayerStatus.ALIVE,
+      role: PlayerRole.PLAYER,
+      roomId: 1,
+      passcode: 1234,
+      targetId: null,
+      missionId: null,
+    },
+  ]);
+}
