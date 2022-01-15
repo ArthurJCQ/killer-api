dda7c870bf3634fb87ab1b83a3ba82e935ff1f11
ArthurJCQ <arthurjcq@hotmail.fr>
2022-01-15 18:25:23 +0100

    :bug: Fix update room

--- src/modules/room/room.repository.ts
+++ src/modules/room/room.repository.ts
@@ -50,14 +50,15 @@ export class RoomRepository {
     { name, status, dateEnd }: UpdateRoomDto,
     code: string,
   ): Promise<RoomModel> {
+    console.log(code);
     const [room] = await this.db
       .client<RoomModel>(ROOM)
+      .where({ code })
       .update({
         name,
         status,
         dateEnd,
       })
-      .where(code)
       .returning('*');
 
     return room;
