323a0c34e0dba417f8d8af66442f43fe65a7656c
Arthur <arthurjcq@hotmail.fr>
2022-04-03 01:49:36 +0200

    :bug: Capitalize all roomCode received in body requests (#39)

--- src/modules/player/dtos/create-player.dto.ts
+++ src/modules/player/dtos/create-player.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString, Length, MinLength } from 'class-validator';
 
+import { Capitalize } from '../../../decorators/capitalize.decorator';
 import { Trim } from '../../../decorators/trim.decorator';
 
 export class CreatePlayerDto {
@@ -9,6 +10,7 @@ export class CreatePlayerDto {
   @Length(5, 5)
   @IsOptional()
   @Trim()
+  @Capitalize()
   roomCode?: string;
 
   @ApiProperty()
