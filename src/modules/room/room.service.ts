05bda7fe806e9d03802491684c2fb76550caf2ca
ArthurJCQ <arthurjcq@hotmail.fr>
2022-06-05 14:22:23 +0200

    :bug: Add data in mercure event

--- src/modules/room/room.service.ts
+++ src/modules/room/room.service.ts
@@ -163,7 +163,11 @@ export class RoomService {
 
     this.eventEmitter.emit(
       'push.mercure',
-      new MercureEvent(`room/${code}`, null, MercureEventType.ROOM_DELETED),
+      new MercureEvent(
+        `room/${code}`,
+        JSON.stringify(code),
+        MercureEventType.ROOM_DELETED,
+      ),
     );
   }
 }
